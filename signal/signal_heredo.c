7a1acdde711399d765185826d2edc9fd0492afd1
psousa <psousa@student.42porto.com>
2023-12-06 03:47:08 +0000

    fix name ffs

--- signal/signal_heredo.c
+++ signal/signal_heredo.c
@@ -14,24 +14,18 @@
 
 void	exec_ctrl_c(int signal, t_status *status)
 {
-	//static *tmp = status;
-	//if (signal = -1)
-	//tmp = status
-	//else
-	//free a tudo
 	(void)signal;
 	write(1, "\n", 1);
 }
-// g_exit_status = EXIT_CTRL_C;
+
 void	exec_ctrl_bslash(int signal)
 {
 	(void)signal;
 	printf("Quit (core dumped)\n");
 }
-// g_exit_status = 131;
+
 void	signals_exec(void)
 {
-
 	signal(SIGINT, exec_ctrl_c);
 	signal(SIGQUIT, exec_ctrl_bslash);
 }
